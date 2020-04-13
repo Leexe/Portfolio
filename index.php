a1135c4911f58502acbfa239761eff35940933d3
Leexe <54720286+Leexe@users.noreply.github.com>
2020-04-13 14:31:38 -0700

    Added files from repl.it portfolio.

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links Tuan Le Period 6</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <li><a href="Le_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Le_Projects.html">Projects.html</a></li></br>
+      <li><a href="Le_Resume.html">Resume.html</a></li></br>
+      <li><a href="Le_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
